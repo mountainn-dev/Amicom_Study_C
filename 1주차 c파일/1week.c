a31f42ecc9c94571418e4648589dd7d45a94e49c
ksh0196 <ksh0196@ajou.ac.kr>
2023-03-19 00:07:58 +0900

    Study_C_1주차

---
+++ 1주차 c파일/1week.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void) {
+	int days = 365;
+	int a1, a2;
+	double rate = 3.1415926535;
+
+	printf("Total days in Year\n");
+	printf("%d\n", days);
+	printf("Circumference rate\n");
+	printf("%lf\n", rate);
+
+	scanf("%d %d", &a1, &a2);
+	printf("%d %d", a1, a2);
+	return 0;
+}

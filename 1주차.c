5dfc184ea5057fdd63cb59e8d78881632afecd3e
DESKTOP-FH84UA5\이상은 <anny0710@naver.com>
2023-03-29 20:17:05 +0900

    [docs] Study_C_2주차

--- 1주차.c
+++ 1주차.c
@@ -1,16 +1,16 @@
-#define _CRT_SECURE_NO_WARNINGS
-#include <stdio.h>
-
-
-int main() {
-	int a;
-	float b;
-
-	printf("Total days in Year\n");
-	scanf("%d", &a);
-	printf("Circumference rate\n");
-	scanf("%f", &b);
-
-	return 0;
-
-}
+//#define _CRT_SECURE_NO_WARNINGS
+//#include <stdio.h>
+//
+//
+//int main() {
+//	int a;
+//	float b;
+//
+//	printf("Total days in Year\n");
+//	scanf("%d", &a);
+//	printf("Circumference rate\n");
+//	scanf("%f", &b);
+//
+//	return 0;
+//
+//}

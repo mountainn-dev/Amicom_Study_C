36f54158775112d25b85976eb4e794302bd84300
chan <ahridva0623@gmail.com>
2023-09-29 19:52:18 +0900

    Week 1 c program upload

---
+++ Amicom_C_1.c
@@ -0,0 +1,10 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(void) {
+	printf("Total days of the Year\n");
+	printf("365\n");
+	printf("Circumference rate\n");
+	printf("3.1415926535\n");
+	return 0;
+}
